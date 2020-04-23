a6543244e11451037f37536cc833384752252d82
Ndumiso Onke Fanti <0nk3.0nk3@gmail.com>
2020-04-23 18:25:53 +0200

    second commit

--- src/simple_calculator.js
+++ src/simple_calculator.js
@@ -1,10 +1,16 @@
 function add(...int){ // rest operator for indefinite arguments
-    let sum = int.reduce((a, b) => a + b);
+    let sum = 0;
+    for (let i = 0; i < int.length; i++){
+        sum += int[i];
+    }
     return sum;
 };
 
 function multiply(...num){
-    let total = num.reduce((x, y) => x * y);
+    let total = 1; // 1 because anything multiplied by 0 is 0
+    for (let i = 0; i < num.length; i++){
+        total *= num[i];
+    }
     return total;
 };
 
